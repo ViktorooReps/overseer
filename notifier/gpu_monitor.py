55971ea29a5cb07671543a890c4cf3c3640a8299
Viktor Scherbakov <viktoroo.sch@gmail.com>
2022-04-09 13:02:13 +0300

    markdown fix

--- notifier/gpu_monitor.py
+++ notifier/gpu_monitor.py
@@ -64,7 +64,7 @@ def _collect_message(status: Tuple[GPUStatus, ...]) -> str:
         utilization = (sum(gpu_status.occupied_memory) / gpu_status.total_memory) * 100
         msg += f'\n\n*GPU{gpu_idx}* `{sum(gpu_status.occupied_memory)}/{gpu_status.total_memory}` Mb ({utilization:.2f}%):'
         if not len(gpu_status.occupied_by):
-            msg += '\n    vacant\\!'
+            msg += '\n    vacant\!'
         for username, memory in zip(gpu_status.occupied_by, gpu_status.occupied_memory):
             description = username_descriptions[username].replace(',,,', '')
             if len(description):
